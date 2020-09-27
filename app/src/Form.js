647331b7f4046cfb045152a1bcbf9e6d40c2038f
Aravinda86 <Aravinda86@users.noreply.github.com>
2020-09-27 14:46:11 -0400

    Final version of App frontend

--- app/src/Form.js
+++ app/src/Form.js
@@ -2,26 +2,32 @@ import React from "react";
 import "./Form.css";
 
 const Form = () => {
-  const [value, setValue] = React.useState("");
+  const [city, setCity] = React.useState("");
   const [country, setCountry] = React.useState("");
 
   const [farenhite, setFarenhite] = React.useState(0);
-  const [celcius, setCelcius] = React.useState(0);
+  const [celcius, setCelcius] = React.useState(0); //to catch the life cycle of a particular parameter
 
-  // const onChange = (event) => setValue(event.target.value);
+  const convertKtoF = (temperature) =>
+    ((temperature - 273.15) * (9 / 5) + 32).toFixed(2);
+  const convertKtoC = (temperature) => (temperature - 273.15).toFixed(2);
+
+  // const onChange = (event) => setCity(event.target.value);
   const getWeather = () => {
-    alert(`Weather ${value}!`);
+    //alert(`Weather ${city}!`);
 
-    if (value != null && value != "" && country != null && country != "") {
+    if (city != null && city !== "") {
       const url = `http://localhost:4000/weather`;
-      // const data = { city: value, country: "uk" };
 
       fetch(url, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ city: value, country }),
+        body: JSON.stringify({
+          city: city,
+          country: country,
+        }),
       })
         .then((response) => response.json())
         .then((data) => {
@@ -29,40 +35,52 @@ const Form = () => {
             "Success:",
             data,
             data.main.temp,
-            ((data.main.temp - 32) * (5 / 9)).toFixed(2)
+            convertKtoF(data.main.temp)
           );
-          setFarenhite(data.main.temp);
-          setCelcius(((data.main.temp - 32) * (5 / 9)).toFixed(2));
+          setFarenhite(convertKtoF(data.main.temp));
+          setCelcius(convertKtoC(data.main.temp));
         })
         .catch((error) => {
           console.error("Error:", error);
         });
     } else {
-      alert(`City and Country cannot be null or blank`);
+      alert(`City cannot be blank`);
     }
   };
 
   return (
-    <div className="form">
-      <label htmlFor="city">City</label>
-      <input
-        id="city"
-        value={value}
-        type="text"
-        onChange={(event) => setValue(event.target.value)}
-      />
-      {/* <button onClick={getWeather}>Get Weather!</button> */}
-      <br />
-      <label htmlFor="city">Country</label>
-      <input
-        id="country"
-        value={country}
-        type="text"
-        onChange={(event) => setCountry(event.target.value)}
-      />
-      <br />
+    <div className="weather_container">
+      <h1 className="weather_h1">Weather App</h1>
+      <div className="weather_form__inputs">
+        <div className="weather_form__labelInput">
+          <div className="weather_form__label">
+            <label htmlFor="city">City</label>
+          </div>
+          <div className="weather_form__input">
+            <input
+              id="city"
+              value={city}
+              type="text"
+              onChange={(event) => setCity(event.target.value)}
+            />
+          </div>
+        </div>
+        {/* <button onClick={getWeather}>Get Weather!</button> */}
+        <div className="weather_form__labelInput">
+          <div className="weather_form__label">
+            <label htmlFor="city">Country</label>
+          </div>
+          <div className="weather_form__input">
+            <input
+              id="country"
+              value={country}
+              type="text"
+              onChange={(event) => setCountry(event.target.value)}
+            />
+          </div>
+        </div>
+      </div>
       <button onClick={getWeather}>Get Weather!</button>
-
       <h3>{farenhite} F</h3>
       <h3>{celcius} C</h3>
     </div>
